1259165446b7b3369788a1301412d7f09ed103d1
rickyroure <127377831+rickyroure@users.noreply.github.com>
2023-03-20 14:17:26 -0300

    initial commit

---
+++ index.js.js
@@ -0,0 +1,44 @@
+function toggleSection1() {
+  var seccion1 = document.getElementById("seccion1");
+  if (seccion1.style.display === "none") {
+    seccion1.style.display = "block";
+  } else {
+    seccion1.style.display = "none";
+  }
+}
+
+function toggleSection2() {
+  var seccion2 = document.getElementById("seccion2");
+  if (seccion2.style.display === "none") {
+    seccion2.style.display = "block";
+  } else {
+    seccion2.style.display = "none";
+  }
+}
+
+function toggleSection3() {
+  var seccion3 = document.getElementById("seccion3");
+  if (seccion3.style.display === "none") {
+    seccion3.style.display = "block";
+  } else {
+    seccion3.style.display = "none";
+  }
+}
+
+function toggleSection4() {
+  var seccion4 = document.getElementById("seccion4");
+  if (seccion4.style.display === "none") {
+    seccion4.style.display = "block";
+  } else {
+    seccion4.style.display = "none";
+  }
+}
+
+function toggleSection5() {
+  var seccion5 = document.getElementById("seccion5");
+  if (seccion5.style.display === "none") {
+    seccion5.style.display = "block";
+  } else {
+    seccion5.style.display = "none";
+  }
+}
